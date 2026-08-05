4967e2b7ff7bfb91aafb8282762d975d525c298e
Danzy-189 <danzypetrykin@gmail.com>
2026-08-06 01:40:31 +0300

    feat: settings for the third person follow camera

--- src/main/java/dev/danzy/cinecam/client/CameraSettings.java
+++ src/main/java/dev/danzy/cinecam/client/CameraSettings.java
@@ -35,6 +35,18 @@ public class CameraSettings {
     /** Aim point height above the player's feet. */
     public double aimHeight = 1.4D;
 
+    // Third person follow camera.
+    /** Length of the spring arm behind the player. */
+    public double followDistance = 4.5D;
+    /** Resting elevation of the arm, in degrees below the horizon. */
+    public float followPitch = 18.0F;
+    /** Sideways offset for an over the shoulder frame. */
+    public double followShoulder = 0.0D;
+    /** 0 = the camera never swings behind the player, 1 = it snaps there. */
+    public float followAlign = 0.35F;
+    /** Pull the camera in instead of letting it clip into blocks. */
+    public boolean followCollision = true;
+
     public void resetDefaults() {
         this.moveSpeed = 0.35D;
         this.fov = 70.0D;
@@ -49,6 +61,11 @@ public class CameraSettings {
         this.orbitHeight = 2.0D;
         this.orbitSpeed = 12.0D;
         this.aimHeight = 1.4D;
+        this.followDistance = 4.5D;
+        this.followPitch = 18.0F;
+        this.followShoulder = 0.0D;
+        this.followAlign = 0.35F;
+        this.followCollision = true;
         this.save();
     }
 
@@ -62,6 +79,10 @@ public class CameraSettings {
         this.orbitHeight = Mth.clamp(this.orbitHeight, -16.0D, 32.0D);
         this.orbitSpeed = Mth.clamp(this.orbitSpeed, -120.0D, 120.0D);
         this.aimHeight = Mth.clamp(this.aimHeight, -2.0D, 4.0D);
+        this.followDistance = Mth.clamp(this.followDistance, 0.5D, 24.0D);
+        this.followPitch = Mth.clamp(this.followPitch, -80.0F, 80.0F);
+        this.followShoulder = Mth.clamp(this.followShoulder, -3.0D, 3.0D);
+        this.followAlign = Mth.clamp(this.followAlign, 0.0F, 1.0F);
     }
 
     public void load() {
@@ -89,6 +110,11 @@ public class CameraSettings {
         this.orbitHeight = readDouble(properties, "orbitHeight", this.orbitHeight);
         this.orbitSpeed = readDouble(properties, "orbitSpeed", this.orbitSpeed);
         this.aimHeight = readDouble(properties, "aimHeight", this.aimHeight);
+        this.followDistance = readDouble(properties, "followDistance", this.followDistance);
+        this.followPitch = (float) readDouble(properties, "followPitch", this.followPitch);
+        this.followShoulder = readDouble(properties, "followShoulder", this.followShoulder);
+        this.followAlign = (float) readDouble(properties, "followAlign", this.followAlign);
+        this.followCollision = readBoolean(properties, "followCollision", this.followCollision);
         this.clampAll();
     }
 
@@ -108,6 +134,11 @@ public class CameraSettings {
         properties.setProperty("orbitHeight", Double.toString(this.orbitHeight));
         properties.setProperty("orbitSpeed", Double.toString(this.orbitSpeed));
         properties.setProperty("aimHeight", Double.toString(this.aimHeight));
+        properties.setProperty("followDistance", Double.toString(this.followDistance));
+        properties.setProperty("followPitch", Float.toString(this.followPitch));
+        properties.setProperty("followShoulder", Double.toString(this.followShoulder));
+        properties.setProperty("followAlign", Float.toString(this.followAlign));
+        properties.setProperty("followCollision", Boolean.toString(this.followCollision));
         Path path = file();
         try {
             Path parent = path.getParent();
